103269dcc3dbdd85f7425981c3f8d1c672da8938
Jason Kridner <jdk@ti.com>
2018-04-30 16:01:31 -0400

    Add beagletester

---
+++ package/beagletester/beagletester.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# beagle-tester
+#
+################################################################################
+
+BEAGLETESTER_VERSION = ad018d82b3331839a258e36c8209150a4770b71f
+BEAGLETESTER_SITE = $(call github,jadonk,beagle-tester,$(BEAGLETESTER_VERSION))
+BEAGLETESTER_INSTALL_STAGING = YES
+BEAGLETESTER_LICENSE = GPLv2
+BEAGLETESTER_LICENSE_FILES = LICENSE
+
+define BEAGLETESTER_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define BEAGLETESTER_INSTALL_STAGING_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) DESTDIR=$(STAGING_DIR) prefix=/usr -C $(@D) install
+endef
+
+define BEAGLETESTER_INSTALL_TARGET_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) DESTDIR=$(TARGET_DIR) prefix=/usr -C $(@D) install
+endef
+
+$(eval $(generic-package))
