109f0768fa39a499eb4ebbc0f295e7572fcf1dde
Jason Kridner <jdk@ti.com>
2018-07-09 15:18:17 -0400

    Update beagle-tester package

--- package/beagletester/beagletester.mk
+++ package/beagletester/beagletester.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BEAGLETESTER_VERSION = ad018d82b3331839a258e36c8209150a4770b71f
+BEAGLETESTER_VERSION = e325eb02062a7a0028c0812aa20c43571b8a2948
 BEAGLETESTER_SITE = $(call github,jadonk,beagle-tester,$(BEAGLETESTER_VERSION))
 BEAGLETESTER_INSTALL_STAGING = YES
 BEAGLETESTER_LICENSE = GPLv2
