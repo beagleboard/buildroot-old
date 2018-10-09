87f2284d3fba9eaad89e705183934e5319653d6e
Jason Kridner <jdk@ti.com>
2018-10-09 11:16:19 -0400

    Fix order of writing things to post-image

--- board/pocketbeagle/post-build.sh
+++ board/pocketbeagle/post-build.sh
@@ -7,13 +7,13 @@ BOARD_DIR="$(dirname $0)"
 # copy the boot files to the target rootfs
 cp $BOARD_DIR/uEnv.txt $TARGET_DIR/
 cp $BOARD_DIR/beaglerc.sh $TARGET_DIR/etc/rc.local
+mkdir -p $TARGET_DIR/usr/share/sounds/alsa/
+cp $BOARD_DIR/Front_Center.wav $TARGET_DIR/usr/share/sounds/alsa/
 mkdir -p $TARGET_DIR/var/local
 cp $BOARD_DIR/usb_image.img $TARGET_DIR/var/local/usb_mass_storage.img
 mkdir -p $TARGET_DIR/boot
 cp $BINARIES_DIR/*.dtb $TARGET_DIR/boot/
 cp $BINARIES_DIR/zImage $TARGET_DIR/boot/
-cd $TARGET_DIR/boot/
-ln -s am335x-boneblack.dtb board.dtb
 echo "$(git show -s --date=short --format='Buildroot beagle-tester %cd %h')" > $TARGET_DIR/etc/dogtag
-mkdir -p $TARGET_DIR/usr/share/sounds/alsa/
-cp $BOARD_DIR/Front_Center.wav $TARGET_DIR/usr/share/sounds/alsa/
+cd $TARGET_DIR/boot/
+ln -f -s am335x-boneblack.dtb board.dtb
